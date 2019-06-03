ee7fca57265e9f54d284801efdaa4aa70a81f5ec
marcus hall <marcush@lovattsmedia.com>
2019-06-03 17:53:56 +1000

    category pages

---
+++ theme/timberwolf/category-projects.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package timberwolf
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area site-max-width">
+		<main id="main" class="site-main">
+
+
+
+	<?php if (have_posts() ) : ?>
+
+			<header class="page-header projects-category-header">
+				
+				<h1 class="projects-category-heading text-center">Projects</h1>
+
+			</header><!-- .page-header -->
+
+
+
+            <div class="project-list">
+
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+				
+
+            endwhile;
+        endif;
+
+        ?>
+        
+        </div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+/* get_sidebar(); */
+get_footer();
---
+++ theme/timberwolf/template-parts/content-project-single.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package timberwolf
+ */
+
+?>
+
+<article class="service-post site-max-width" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<h3>Content-Project Template (single)</h3>
+
+	<div class="pubication-flex-wrapper">
+
+		<div class="service-cover">
+			<?php timberwolf_post_thumbnail(); ?>
+		</div>
+
+
+		<div class="service-rte">
+
+			<header class="entry-header">
+				<?php
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				?>
+
+
+			</header><!-- .entry-header -->
+
+			<div class="entry-content">
+				<?php
+				the_content( sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'timberwolf' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				) );
+
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'timberwolf' ),
+					'after'  => '</div>',
+				) );
+				?>
+
+
+
+
+			</div><!-- .entry-content -->
+		</div>
+
+
+	</div>
+
+
+
+
+
+	<footer class="entry-footer">
+		<?php timberwolf_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
